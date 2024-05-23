c7a328238e20466ebc09751d5411d89a463f4216
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-05-22 21:35:42 -0600

    Create pluginMV.java

---
+++ src/main/java/Creadores/Program/pluginMV.java
@@ -0,0 +1,9 @@
+package Creadores.Program;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.event.Listener;
+public class pluginMV extends PluginBase implements Listener{
+  @Override
+  public void onEnable(){
+    this.getLogger().info("§eLoading...");
+  }
+}
